2b87edbec2960ade30d7e9eed6482bd05b5714ae
RafeeqSt <rafeeqstephens@gmail.com>
2019-02-14 00:20:31 +0200

    Add files via upload

---
+++ AppTest.java
@@ -0,0 +1,28 @@
+package za.ac.cput.Projects;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        int num1 = 1;
+        int num2 = 2;
+        int ans;
+
+        ans = num1 + num2;
+
+        System.out.println("The answer to the mathematical problem of 1 + 2 is: " +ans);
+
+        assertTrue( true );
+    }
+}
